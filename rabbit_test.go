75c8c46be4f96dc69db30752774af690c0ad6dc5
Win Htaik Aung <winhtaikaung28@hotmail.com>
2018-10-14 12:07:08 +0800

    Update rabbit_test.go

--- rabbit_test.go
+++ rabbit_test.go
@@ -3,7 +3,7 @@ package main
 import (
 	"testing"
 
-	"github.com/winhtaikaung/rabbit-go/rabbit"
+	"github.com/winhtaikaung/Rabbit-Go/rabbit"
 )
 
 const UNI_STRING = string("သီဟိုဠ်မှ ဉာဏ်ကြီးရှင်သည် အာယုဝဍ္ဎနဆေးညွှန်းစာကို ဇလွန်ဈေးဘေး ဗာဒံပင်ထက် အဓိဋ္ဌာန်လျက် ဂဃနဏဖတ်ခဲ့သည်။")

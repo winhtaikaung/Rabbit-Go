606c215942538c691e5a5d589c6fd9e2cb324ce6
Aung Moe Hein <ang.m.hein@frontiir.net>
2019-03-12 17:27:21 +0630

    solving unicode bug

--- rabbit_test.go
+++ rabbit_test.go
@@ -9,17 +9,29 @@ import (
 const UNI_STRING = string("သီဟိုဠ်မှ ဉာဏ်ကြီးရှင်သည် အာယုဝဍ္ဎနဆေးညွှန်းစာကို ဇလွန်ဈေးဘေး ဗာဒံပင်ထက် အဓိဋ္ဌာန်လျက် ဂဃနဏဖတ်ခဲ့သည်။")
 const ZG_STRING = string("သီဟိုဠ္မွ ဉာဏ္ႀကီးရွင္သည္ အာယုဝၯနေဆးညႊန္းစာကို ဇလြန္ေဈးေဘး ဗာဒံပင္ထက္ အဓိ႒ာန္လ်က္ ဂဃနဏဖတ္ခဲ့သည္။")
 
+const uni_string = string("ငါးပို့မှု")
+const zg_string = string("ငါးပို႔မႈ")
+
 func TestUni2zg(t *testing.T) {
 	if rabbit.Uni2zg(UNI_STRING) != ZG_STRING {
 		t.Log("Should match with Zawgyi string")
 		t.Fail()
 	}
 
+	if rabbit.Uni2zg(uni_string) != zg_string {
+		t.Log("Should match with zawgyi string")
+		t.Fail()
+	}
 }
 
 func TestZg2uni(t *testing.T) {
 	if rabbit.Zg2uni(ZG_STRING) != UNI_STRING {
 		t.Log("Should match with Unicode string")
+		t.Fail()
+	}
+
+	if rabbit.Zg2uni(zg_string) != uni_string {
+		t.Log("Should match with unicode string")
 		t.Fail()
 	}
 }
